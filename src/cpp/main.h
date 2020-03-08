5f0d91230b5837ba72ce37a4896b9c4dd0c4f697
Dario <dario.rekowski@gmx.de>
2020-03-08 08:15:10 +0100

    adding file formats to doxygen main page

--- src/cpp/main.h
+++ src/cpp/main.h
@@ -63,6 +63,42 @@
 *
 * \section file_formats_sec File Formats
 * 
+* root folder is ~/.gradido like with bitcoin or other crypto currencies\n 
+* first file is group.index\n 
+* txt format, created by user\n 
+* containing in this format: \n 
+* &lt;group public key&gt; = &lt;folder name&gt;\n 
+* The Gradido Node create the folder with this name and store every transaction belonging to this group in there\n 
+* in the group folder:\n 
+* &nbsp;<b>.state: leveldb format, containing:</b>\n 
+* <ul>
+*  <li>current group states</li>
+*  <li>counter for account index (lastKtoIndex)</li>
+*  <li>counter for last block id (lastBlockNr)</li>
+* </ul>\n 
+* &nbsp;<b>pubkeys_&lt;first hash byte hex&gt;</b>\n 
+* &nbsp;&nbsp;<b>_&lt;second hash byte hex&gt;.index: binary, unordered, containing:</b>\n 
+* <ul>
+*  <li>account hash, kto index uint32</li>
+*  <li>file hash at end of file</li>
+* </ul>
+* &nbsp;&nbsp;file is unordered, sorting in memory\n 
+* \n
+* &nbsp;<b>blk0000000X.index, binary, sorted by kto index, containing:</b>\n 
+* <ul>
+*  <li>lowest kto index, highest kto index</li>
+*  <li>kto index, count, [count X file positions where transaction which contain kto starts]</li>
+*  <li>file hash at end of file</li>
+* </ul>
+* &nbsp;write out after block finished, until then exist only in memory\n
+* \n
+* &nbsp;<b>blk0000000X.data, binary, sorted by transaction id, containing: </b>\n
+* <ul>
+*  <li>size, serialized transaction (protobuf format)</li>
+*  <li>file hash at end of file</li>
+* </ul>
+* 
+*
 * \subsection step1 Step 1: Opening the box
 *
 * etc...
