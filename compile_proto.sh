261e83c573d71cb8a874f12367f492a8bb854560
Dario <dario.rekowski@gmx.de>
2020-07-28 16:02:45 +0200

    fix bug with proto

--- compile_proto.sh
+++ compile_proto.sh
@@ -6,4 +6,12 @@ if [ ! -d "./src/cpp/proto/gradido" ] ; then
 	mkdir ./src/cpp/proto/gradido
 fi
 
-protoc --cpp_out=./src/cpp/proto/gradido --proto_path=./src/proto/gradido ./src/proto/gradido/*.proto
+protoc --cpp_out=./src/cpp/proto --proto_path=./src/proto ./src/proto/gradido/*.proto
+
+if [ ! -d "./src/cpp/proto/hedera" ] ; then
+	mkdir ./src/cpp/proto/hedera
+fi
+
+protoc --cpp_out=./src/cpp/proto/hedera --proto_path=./src/proto/hedera/hedera-protobuf/src/main/proto ./src/proto/hedera/hedera-protobuf/src/main/proto/*.proto
+
+
