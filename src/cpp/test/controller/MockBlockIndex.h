920030dd3ddd6368b9d093caa35ad0033d8074ac
einhorn_b <dariofrodo@gmx.de>
2021-10-20 13:18:40 +0200

    fix syntax error

--- src/cpp/test/controller/MockBlockIndex.h
+++ src/cpp/test/controller/MockBlockIndex.h
@@ -2,16 +2,16 @@
 #define __GRADIDO_NODE_TEST_CONTROLLER_BLOCK_INDEX_MOCK_H
 
 //#define GTEST_LINKED_AS_SHARED_LIBRARY
-#include "gmock/gmock.h"  // Brings in gMock.
+//#include "gmock/gmock.h"  // Brings in gMock.
 #include "../../model/files/BlockIndex.h"
 #include "../../model/TransactionEntry.h"
 
 namespace controller {
 	class MockBlockIndex : public model::files::IBlockIndexReceiver {
-	public: 
+	public:
 
 		bool addIndicesForTransaction(uint16_t year, uint8_t month, uint64_t transactionNr, const uint32_t* addressIndices, uint8_t addressIndiceCount) {
-			
+
 			mTransactionEntrys.push_back(new model::TransactionEntry(
 				transactionNr, month, year, addressIndices, addressIndiceCount
 			));
@@ -19,15 +19,15 @@ namespace controller {
 		}
 
 		std::vector < Poco::SharedPtr<model::TransactionEntry>> mTransactionEntrys;
-	protected: 
-		
+	protected:
+
 
 	};
 }
 
 /*class MockTurtle : public Turtle {
 public:
-	
+
 	MOCK_METHOD(void, PenUp, (), (override));
 	MOCK_METHOD(void, PenDown, (), (override));
 	MOCK_METHOD(void, Forward, (int distance), (override));
