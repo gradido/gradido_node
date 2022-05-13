f37b7d198cb13185f3deb31a7cf5dc046580468d
einhornimmond <dario.rekowski@gmx.de>
2022-05-13 17:35:45 +0200

    use blake2b from serialized transaction hash as messageId

--- src/controller/ArchiveTransactionsOrdering.cpp
+++ src/controller/ArchiveTransactionsOrdering.cpp
@@ -1,5 +1,6 @@
 #include "ArchiveTransactionsOrdering.h"
 #include "../SingletonManager/LoggerManager.h"
+#include "gradido_blockchain/MemoryManager.h"
 #include "Group.h"
 #include "Poco/Util/ServerApplication.h"
 
@@ -31,41 +32,57 @@ namespace controller {
 	int ArchiveTransactionsOrdering::ThreadFunction()
 	{
 		PendingTransactionsMap::iterator it;
-		{
-			std::scoped_lock<std::shared_mutex> _lock(mPendingTransactionsMutex);
-			if (!mPendingTransactions.size()) return 0;
-			it = mPendingTransactions.begin();
-		}
-		auto lastTransaction = mParentGroup->getLastTransaction();
-		if (!lastTransaction.isNull()) {
-			if (lastTransaction->getID() + 1 != it->first) {
+		while(true)
+		{			
+			{
+				std::scoped_lock<std::shared_mutex> _lock(mPendingTransactionsMutex);
+				if (!mPendingTransactions.size()) return 0;
+				it = mPendingTransactions.begin();
+			}
+			auto lastTransaction = mParentGroup->getLastTransaction();
+			if (!lastTransaction.isNull()) {
+				if (lastTransaction->getID() + 1 != it->first) {
+					return 0;
+				}
+			}
+			else if (it->first > 1) {
 				return 0;
 			}
-		}
-		else if (it->first > 1) {
-			return 0;
-		}
-		try {
-			auto createdTimestamp = it->second->getTransactionBody()->getCreatedSeconds();
-			mParentGroup->addTransaction(std::move(it->second), nullptr, createdTimestamp);
-		}
-		catch (GradidoBlockchainException& ex) {
-			LoggerManager::getInstance()->mErrorLogging.critical("[ArchiveTransactionsOrdering] terminate with gradido blockchain exception: %s", ex.getFullString());
-			Poco::Util::ServerApplication::terminate();
-		}
-		catch (Poco::Exception& ex) {
-			LoggerManager::getInstance()->mErrorLogging.critical("[ArchiveTransactionsOrdering] terminate with poco exception: %s", ex.displayText());
-			Poco::Util::ServerApplication::terminate();
-		}
-		catch (std::exception& ex) {
-			LoggerManager::getInstance()->mErrorLogging.critical("[ArchiveTransactionsOrdering] terminate with exception: %s", std::string(ex.what()));
-			Poco::Util::ServerApplication::terminate();
-		}
-		{
-			std::scoped_lock<std::shared_mutex> _lock(mPendingTransactionsMutex);
-			mPendingTransactions.erase(it);
+			try {
+				auto createdTimestamp = it->second->getTransactionBody()->getCreatedSeconds();
+				// calculate hash with blake2b
+				auto mm = MemoryManager::getInstance();
+				auto hash = mm->getMemory(crypto_generichash_BYTES);
+				auto rawMessage = it->second->getSerialized();
+
+				crypto_generichash(
+					*hash, hash->size(),
+					(const unsigned char*)rawMessage->data(),
+					rawMessage->size(),
+					NULL, 0
+				);
+				mParentGroup->addTransaction(std::move(it->second), hash, createdTimestamp);
+				mm->releaseMemory(hash);
+			}
+			catch (GradidoBlockchainException& ex) {
+				LoggerManager::getInstance()->mErrorLogging.critical("[ArchiveTransactionsOrdering] terminate with gradido blockchain exception: %s", ex.getFullString());
+				Poco::Util::ServerApplication::terminate();
+			}
+			catch (Poco::Exception& ex) {
+				LoggerManager::getInstance()->mErrorLogging.critical("[ArchiveTransactionsOrdering] terminate with poco exception: %s", ex.displayText());
+				Poco::Util::ServerApplication::terminate();
+			}
+			catch (std::exception& ex) {
+				LoggerManager::getInstance()->mErrorLogging.critical("[ArchiveTransactionsOrdering] terminate with exception: %s", std::string(ex.what()));
+				Poco::Util::ServerApplication::terminate();
+			}
+			{
+				std::scoped_lock<std::shared_mutex> _lock(mPendingTransactionsMutex);
+				mPendingTransactions.erase(it);
+			}
 		}
 
+		return 0;
 	}
 
 	// *************** Exceptions ****************************
